de9c819b9ba2cfe5ace713f628140b90707c46f6
fengzebing <1390653296@qq.com>
2024-08-23 20:12:49 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ git add .
 git commit -m 'deploy'
 
 # 部署到 https://<username>.github.io/<repo>
-git push -f https://github.com/exploitationExplorers/Leetcode-project.git main:github-pages
+git push -f https://github.com/exploitationExplorers/Leetcode-project.git master:github-pages
 
 # 提交所有代码到github
 cd ../../../
